e89dfa3a63cdf538583164ab36de8be12e82dc5a
Kasidi Bellanger <ksbellanger@gmail.com>
2017-04-12 18:28:13 -0600

    Added Empty Xcode Project

---
+++ PixieProductsMobileApp/PixieProductsMobileApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PixieProductsMobileApp
+//
+//  Created by Kasidi Bellanger on 2017-04-12.
+//  Copyright © 2017 Kasidi Bellanger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PixieProductsMobileApp/PixieProductsMobileApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PixieProductsMobileApp
+//
+//  Created by Kasidi Bellanger on 2017-04-12.
+//  Copyright © 2017 Kasidi Bellanger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
